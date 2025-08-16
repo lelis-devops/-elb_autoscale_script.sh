16c4fe9d0b892e0c11fadaeaa474e05a834e144b
Mileni Lelis Ramos Sérvulo <lelis.engineer@yahoo.com>
2025-08-15 21:56:08 -0300

    Update elb_autoscale.sh

--- elb_autoscale.sh
+++ elb_autoscale.sh
@@ -3,26 +3,31 @@
 INSTANCE_TYPE="t3.micro"
 AVAILABILITY_ZONE="sa-east-1a"
 AMI="ami-0d1b5a8c13042c939"
-FIREWALL="sg-09c51fa83b004aad1"
+SECURITY_GROUP="sg-09c51fa83b004aad1"
 KEY_NAME="girl"
 VOLUME_SIZE=20
 ROLE_NAME="SSM"
+
 create_EC2() {
     INSTANCE_ID=$(aws ec2 run-instances \
         --image-id "$AMI" \
         --instance-type "$INSTANCE_TYPE" \
         --placement AvailabilityZone="$AVAILABILITY_ZONE" \
         --key-name "$KEY_NAME" \
-        --security-group-ids "$FIREWALL" \
-      --iam-instance-profile "$ROLE_NAME"
-    --block-device-mappings "[{\"DeviceName\":\"/dev/sdf\",\"Ebs\":{\"VolumeSize\":$VOLUME_SIZE}}]" \
-   --user-data '#!/bin/bash    
-sudo snap start amazon-ssm-agent
-sudo snap enable amazon-ssm-agent
-sudo snap services amazon-ssm-agent apt update -y' \
-       
+        --security-group-ids "$SECURITY_GROUP" \
+        --iam-instance-profile Name="$ROLE_NAME" \
+        --block-device-mappings "[{\"DeviceName\":\"/dev/sdf\",\"Ebs\":{\"VolumeSize\":$VOLUME_SIZE}}]" \
+        --user-data '#!/bin/bash
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+sudo apt update -y' \
         --query "Instances[0].InstanceId" \
         --output text)
+
     export INSTANCE_ID
+    echo "Instance created: $INSTANCE_ID"
+
     aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+    echo "Instance is running, starting SSM session..."
+    aws ssm start-session --target "$INSTANCE_ID"
 }
