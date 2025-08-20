eac231a74d7bfc28a08ed1e4609f974319d0ca68
Mileni Lelis Ramos Sérvulo <lelis.engineer@yahoo.com>
2025-08-19 22:26:05 -0300

    Update elb_autoscale.sh

--- elb_autoscale.sh
+++ elb_autoscale.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
-INSTANCE_TYPE="t3.micro"
-AVAILABILITY_ZONE="sa-east-1a"
-AMI="ami-0d1b5a8c13042c939"
-SECURITY_GROUP="sg-09c51fa83b004aad1"
-KEY_NAME="girl"
-VOLUME_SIZE=20
-ROLE_NAME="SSM"
+export PORT=3000
+export DB_HOST=localhost
+export DB_PORT=5329
+export DB_USER=mariadb
+export DB_PASSWORD=123
+export DB_NAME=catalogo
 
-create_EC2() {
-    INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id "$AMI" \
-        --instance-type "$INSTANCE_TYPE" \
-        --placement AvailabilityZone="$AVAILABILITY_ZONE" \
-        --key-name "$KEY_NAME" \
-        --security-group-ids "$SECURITY_GROUP" \
-        --iam-instance-profile Name="$ROLE_NAME" \
-        --block-device-mappings "[{\"DeviceName\":\"/dev/sdf\",\"Ebs\":{\"VolumeSize\":$VOLUME_SIZE}}]" \
-        --user-data '#!/bin/bash
-sudo systemctl enable amazon-ssm-agent
-sudo systemctl start amazon-ssm-agent
-sudo apt update -y' \
-        --query "Instances[0].InstanceId" \
-        --output text)
 
-    export INSTANCE_ID
-    echo "Instance created: $INSTANCE_ID"
+export PGPASSWORD=$DB_PASSWORD
 
-    aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
-    echo "Instance is running, starting SSM session..."
-    aws ssm start-session --target "$INSTANCE_ID"
+echo "Variáveis de ambiente configuradas."
+mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASSWORD $DB_NAME
+
+
+
+ backend () {
+ aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 314296197182.dkr.ecr.us-east-2.amazonaws.com
+docker build -t bootcamp .
+docker tag bootcamp:latest 314296197182.dkr.ecr.us-east-2.amazonaws.com/bootcamp:latest
+docker push 314296197182.dkr.ecr.us-east-2.amazonaws.com/bootcamp:latest
 }
+
+frontend () {
+puxar arquivo front pelo amplify
+} 
